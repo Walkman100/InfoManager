2cc6d2c62a2df63d2d17ba1a4516caf5ee54392e
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2014-11-07 23:49:57 +0200

    Added basic code

--- InfoManager.Designer.vb
+++ InfoManager.Designer.vb
@@ -39,6 +39,7 @@ Partial Class InfoManager
         Me.txtData2 = New System.Windows.Forms.TextBox()
         Me.StatusStrip = New System.Windows.Forms.StatusStrip()
         Me.StatusStripStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.txtName = New System.Windows.Forms.TextBox()
         Me.StatusStrip.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -49,16 +50,17 @@ Partial Class InfoManager
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lstPrograms.FormattingEnabled = True
         Me.lstPrograms.IntegralHeight = False
+        Me.lstPrograms.Items.AddRange(New Object() {"Program1", "Program2", "Program3", "Program4", "Program5", "Program6", "Program7", "Program8", "Program9", "Add..."})
         Me.lstPrograms.Location = New System.Drawing.Point(12, 12)
         Me.lstPrograms.Name = "lstPrograms"
-        Me.lstPrograms.Size = New System.Drawing.Size(120, 227)
+        Me.lstPrograms.Size = New System.Drawing.Size(120, 253)
         Me.lstPrograms.TabIndex = 0
         '
         'txtData1
         '
         Me.txtData1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txtData1.Enabled = False
-        Me.txtData1.Location = New System.Drawing.Point(138, 25)
+        Me.txtData1.Location = New System.Drawing.Point(138, 51)
         Me.txtData1.Name = "txtData1"
         Me.txtData1.Size = New System.Drawing.Size(134, 20)
         Me.txtData1.TabIndex = 1
@@ -68,7 +70,7 @@ Partial Class InfoManager
         Me.lblData1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lblData1.AutoSize = True
         Me.lblData1.Enabled = False
-        Me.lblData1.Location = New System.Drawing.Point(138, 9)
+        Me.lblData1.Location = New System.Drawing.Point(138, 35)
         Me.lblData1.Name = "lblData1"
         Me.lblData1.Size = New System.Drawing.Size(33, 13)
         Me.lblData1.TabIndex = 2
@@ -79,7 +81,7 @@ Partial Class InfoManager
         Me.lblButton1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lblButton1.AutoSize = True
         Me.lblButton1.Enabled = False
-        Me.lblButton1.Location = New System.Drawing.Point(138, 48)
+        Me.lblButton1.Location = New System.Drawing.Point(138, 74)
         Me.lblButton1.Name = "lblButton1"
         Me.lblButton1.Size = New System.Drawing.Size(81, 13)
         Me.lblButton1.TabIndex = 3
@@ -92,7 +94,7 @@ Partial Class InfoManager
         Me.cbxButton1.Enabled = False
         Me.cbxButton1.FormattingEnabled = True
         Me.cbxButton1.Items.AddRange(New Object() {"Enter", "Tab", "Space", "Down Arrow", "Right Arrow", "Custom..."})
-        Me.cbxButton1.Location = New System.Drawing.Point(138, 64)
+        Me.cbxButton1.Location = New System.Drawing.Point(138, 90)
         Me.cbxButton1.Name = "cbxButton1"
         Me.cbxButton1.Size = New System.Drawing.Size(134, 21)
         Me.cbxButton1.TabIndex = 4
@@ -101,7 +103,7 @@ Partial Class InfoManager
         '
         Me.txtButton1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txtButton1.Enabled = False
-        Me.txtButton1.Location = New System.Drawing.Point(138, 91)
+        Me.txtButton1.Location = New System.Drawing.Point(138, 117)
         Me.txtButton1.Name = "txtButton1"
         Me.txtButton1.Size = New System.Drawing.Size(134, 20)
         Me.txtButton1.TabIndex = 5
@@ -109,7 +111,7 @@ Partial Class InfoManager
         'btnRun
         '
         Me.btnRun.Anchor = System.Windows.Forms.AnchorStyles.Bottom
-        Me.btnRun.Location = New System.Drawing.Point(93, 245)
+        Me.btnRun.Location = New System.Drawing.Point(93, 271)
         Me.btnRun.Name = "btnRun"
         Me.btnRun.Size = New System.Drawing.Size(98, 23)
         Me.btnRun.TabIndex = 6
@@ -119,7 +121,7 @@ Partial Class InfoManager
         'btnEnd
         '
         Me.btnEnd.Anchor = System.Windows.Forms.AnchorStyles.Bottom
-        Me.btnEnd.Location = New System.Drawing.Point(197, 245)
+        Me.btnEnd.Location = New System.Drawing.Point(197, 271)
         Me.btnEnd.Name = "btnEnd"
         Me.btnEnd.Size = New System.Drawing.Size(75, 23)
         Me.btnEnd.TabIndex = 7
@@ -129,7 +131,7 @@ Partial Class InfoManager
         'btnSave
         '
         Me.btnSave.Anchor = System.Windows.Forms.AnchorStyles.Bottom
-        Me.btnSave.Location = New System.Drawing.Point(12, 245)
+        Me.btnSave.Location = New System.Drawing.Point(12, 271)
         Me.btnSave.Name = "btnSave"
         Me.btnSave.Size = New System.Drawing.Size(75, 23)
         Me.btnSave.TabIndex = 8
@@ -141,7 +143,7 @@ Partial Class InfoManager
         Me.chkMultiLine.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.chkMultiLine.AutoSize = True
         Me.chkMultiLine.Enabled = False
-        Me.chkMultiLine.Location = New System.Drawing.Point(138, 117)
+        Me.chkMultiLine.Location = New System.Drawing.Point(138, 143)
         Me.chkMultiLine.Name = "chkMultiLine"
         Me.chkMultiLine.Size = New System.Drawing.Size(71, 17)
         Me.chkMultiLine.TabIndex = 9
@@ -152,7 +154,7 @@ Partial Class InfoManager
         '
         Me.txtButton2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txtButton2.Enabled = False
-        Me.txtButton2.Location = New System.Drawing.Point(138, 219)
+        Me.txtButton2.Location = New System.Drawing.Point(138, 245)
         Me.txtButton2.Name = "txtButton2"
         Me.txtButton2.Size = New System.Drawing.Size(134, 20)
         Me.txtButton2.TabIndex = 14
@@ -164,7 +166,7 @@ Partial Class InfoManager
         Me.cbxButton2.Enabled = False
         Me.cbxButton2.FormattingEnabled = True
         Me.cbxButton2.Items.AddRange(New Object() {"Enter", "Tab", "Space", "Down Arrow", "Right Arrow", "Custom..."})
-        Me.cbxButton2.Location = New System.Drawing.Point(138, 192)
+        Me.cbxButton2.Location = New System.Drawing.Point(138, 218)
         Me.cbxButton2.Name = "cbxButton2"
         Me.cbxButton2.Size = New System.Drawing.Size(134, 21)
         Me.cbxButton2.TabIndex = 13
@@ -174,7 +176,7 @@ Partial Class InfoManager
         Me.lblButton2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lblButton2.AutoSize = True
         Me.lblButton2.Enabled = False
-        Me.lblButton2.Location = New System.Drawing.Point(138, 176)
+        Me.lblButton2.Location = New System.Drawing.Point(138, 202)
         Me.lblButton2.Name = "lblButton2"
         Me.lblButton2.Size = New System.Drawing.Size(81, 13)
         Me.lblButton2.TabIndex = 12
@@ -185,7 +187,7 @@ Partial Class InfoManager
         Me.lblData2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lblData2.AutoSize = True
         Me.lblData2.Enabled = False
-        Me.lblData2.Location = New System.Drawing.Point(138, 137)
+        Me.lblData2.Location = New System.Drawing.Point(138, 163)
         Me.lblData2.Name = "lblData2"
         Me.lblData2.Size = New System.Drawing.Size(33, 13)
         Me.lblData2.TabIndex = 11
@@ -195,7 +197,7 @@ Partial Class InfoManager
         '
         Me.txtData2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txtData2.Enabled = False
-        Me.txtData2.Location = New System.Drawing.Point(138, 153)
+        Me.txtData2.Location = New System.Drawing.Point(138, 179)
         Me.txtData2.Name = "txtData2"
         Me.txtData2.Size = New System.Drawing.Size(134, 20)
         Me.txtData2.TabIndex = 10
@@ -203,7 +205,7 @@ Partial Class InfoManager
         'StatusStrip
         '
         Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StatusStripStatusLabel})
-        Me.StatusStrip.Location = New System.Drawing.Point(0, 271)
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 297)
         Me.StatusStrip.Name = "StatusStrip"
         Me.StatusStrip.Size = New System.Drawing.Size(284, 22)
         Me.StatusStrip.TabIndex = 15
@@ -216,11 +218,21 @@ Partial Class InfoManager
         Me.StatusStripStatusLabel.Text = "Select an item to start..."
         Me.StatusStripStatusLabel.ToolTipText = "Status"
         '
+        'txtName
+        '
+        Me.txtName.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtName.Enabled = False
+        Me.txtName.Location = New System.Drawing.Point(138, 12)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(134, 20)
+        Me.txtName.TabIndex = 16
+        '
         'InfoManager
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 293)
+        Me.ClientSize = New System.Drawing.Size(284, 319)
+        Me.Controls.Add(Me.txtName)
         Me.Controls.Add(Me.StatusStrip)
         Me.Controls.Add(Me.txtButton2)
         Me.Controls.Add(Me.cbxButton2)
@@ -262,5 +274,6 @@ Partial Class InfoManager
     Friend WithEvents txtData2 As System.Windows.Forms.TextBox
     Friend WithEvents StatusStrip As System.Windows.Forms.StatusStrip
     Friend WithEvents StatusStripStatusLabel As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
 
 End Class
--- InfoManager.vb
+++ InfoManager.vb
@@ -1,3 +1,114 @@
-﻿Public Class InfoManager
+﻿Imports Microsoft.VisualBasic
 
+Public Class InfoManager
+
+    Private Sub InfoManager_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lstPrograms_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstPrograms.SelectedIndexChanged
+        Select Case lstPrograms.SelectedIndex + 1
+            Case 1 'Program1
+                EnableGUI()
+                txtName.Text = My.Settings.Program1.Item(0)
+            Case 2 'Program2
+                EnableGUI()
+                txtName.Text = My.Settings.Program2.Item(0)
+            Case 3 'Program3
+                EnableGUI()
+                txtName.Text = My.Settings.Program3.Item(0)
+            Case 4 'Program4
+                EnableGUI()
+                txtName.Text = My.Settings.Program4.Item(0)
+            Case 5 'Program5
+                EnableGUI()
+                txtName.Text = My.Settings.Program5.Item(0)
+            Case 6 'Program6
+                EnableGUI()
+                txtName.Text = My.Settings.Program6.Item(0)
+            Case 7 'Program7
+                EnableGUI()
+                txtName.Text = My.Settings.Program7.Item(0)
+            Case 8 'Program8
+                EnableGUI()
+                txtName.Text = My.Settings.Program8.Item(0)
+            Case 9 'Program9
+                EnableGUI()
+                txtName.Text = My.Settings.Program9.Item(0)
+            Case 10 'Program0 (Extendable)
+                txtName.Enabled = False
+                lblData1.Enabled = False
+                txtData1.Enabled = False
+                lblButton1.Enabled = False
+                cbxButton1.Enabled = False
+                txtButton1.Enabled = False
+                chkMultiLine.Enabled = False
+                lblData2.Enabled = False
+                txtData2.Enabled = False
+                lblButton2.Enabled = False
+                cbxButton2.Enabled = False
+                txtButton2.Enabled = False
+                txtName.Text = My.Settings.Program0.Item(0)
+            Case -1 'Nothing selected
+                txtName.Enabled = False
+                lblData1.Enabled = False
+                txtData1.Enabled = False
+                lblButton1.Enabled = False
+                cbxButton1.Enabled = False
+                txtButton1.Enabled = False
+                chkMultiLine.Enabled = False
+                lblData2.Enabled = False
+                txtData2.Enabled = False
+                lblButton2.Enabled = False
+                cbxButton2.Enabled = False
+                txtButton2.Enabled = False
+        End Select
+    End Sub
+
+    Sub EnableGUI()
+        txtName.Enabled = True
+        lblData1.Enabled = True
+        txtData1.Enabled = True
+        lblButton1.Enabled = True
+        cbxButton1.Enabled = True
+        txtButton1.Enabled = True
+        chkMultiLine.Enabled = True
+        chkMultiLine_CheckedChanged()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+    End Sub
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+
+    End Sub
+
+    Private Sub btnEnd_Click(sender As Object, e As EventArgs) Handles btnEnd.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub cbxButton1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxButton1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub chkMultiLine_CheckedChanged(Optional sender As Object = Nothing, Optional e As EventArgs = Nothing) Handles chkMultiLine.CheckedChanged
+        If chkMultiLine.Checked = True Then
+            lblData2.Enabled = True
+            txtData2.Enabled = True
+            lblButton2.Enabled = True
+            cbxButton2.Enabled = True
+            txtButton2.Enabled = True
+        Else
+            lblData2.Enabled = False
+            txtData2.Enabled = False
+            lblButton2.Enabled = False
+            cbxButton2.Enabled = False
+            txtButton2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub cbxButton2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxButton2.SelectedIndexChanged
+
+    End Sub
 End Class
